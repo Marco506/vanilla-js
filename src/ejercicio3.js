bf9b026ae891887cf0ad7496df5bdc04251e2909
Marco506 <mmedina@fwdcostarica.com>
2024-08-14 15:25:33 -0600

    Avance

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,105 @@
-// Inserte el código aquí
+function clonarArreglo(arreglo) {
+    return [...arreglo];
+};
+const arreglo = ["Marco Medina"];
+const arregloClonado = clonarArreglo(arreglo);
+console.log("arreglo clonado es",arregloClonado);
+
+
+
+
+function clonarObjeto(objeto) {
+    return {...objeto}
+}
+const objeto = {
+    nombre: "Antonio",
+    edad: 22
+}
+const objetoClonado = clonarObjeto(objeto);
+console.log("El objeto clonado es", objetoClonado);
+
+
+
+
+function sumar(...numeros){
+return numeros.reduce
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const names = ["Marco", "Juan", "Luis"];
+
+function clonarArreglo(names){ 
+
+
+clonar = names.map((x)=>x=x);
+return clonar;
+
+
+};
+
+const obje = {
+    nombre : "Marco"
+}
+function clonarObjeto(X){
+    return X;
+}
+
+console.log(clonarArreglo(names));
+console.log(clonarObjeto(obje));
 
 
 
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+//export { clonarArreglo, clonarObjeto, sumar, saludar };
+*/
